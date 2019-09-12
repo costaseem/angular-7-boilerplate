7d6ff3e55b6203120218bf38a779d7144e404791
Guilherme Jacob <costa.seem@gmail.com>
2019-09-12 09:26:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# angular-7-boilerplate
+
+Angular 7 Boilerplate using webpack 4 with login/registration example
+
+To run project use
+
+yarn && yarn start 
---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from '@/_modules/home';
+import { LoginComponent } from '@/_modules/login';
+import { RegisterComponent } from '@/_modules/register';
+import { AuthGuard } from '@/_guards';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
